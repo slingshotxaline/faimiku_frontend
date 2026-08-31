2bc0318fa9b59649f85bb274274e6830a6688ee1
Shahariarshadhin <shadhin19shahriar@gmail.com>
2026-08-31 15:58:36 +0600

    added about page and add location map in footer

--- app/(shop)/about/page.jsx
+++ app/(shop)/about/page.jsx
@@ -2,20 +2,21 @@
 
 import { useGetPageBySlugQuery } from "../../../features/cms/pagesApi";
 import PageSections from "../../../components/shop/PageSections";
+import AboutUs from "../../../components/AboutUs/AboutUs";
 
 export default function AboutPage() {
-  const { data, isLoading } = useGetPageBySlugQuery("about");
-  const page = data?.data;
+  // const { data, isLoading } = useGetPageBySlugQuery("about");
+  // const page = data?.data;
 
-  if (isLoading)
-    return <div className="max-w-3xl mx-auto px-4 py-16">Loading...</div>;
-  if (!page)
-    return <div className="max-w-3xl mx-auto px-4 py-16">Page not found.</div>;
+  // if (isLoading)
+  //   return <div className="max-w-3xl mx-auto px-4 py-16">Loading...</div>;
+  // if (!page)
+  //   return <div className="max-w-3xl mx-auto px-4 py-16">Page not founds.</div>;
 
   return (
-    <div className="max-w-3xl mx-auto px-4 py-10">
-      <h1 className="text-3xl font-bold text-gray-900 mb-8">{page.title}</h1>
-      <PageSections sections={page.sections} />
+    <div className="max-w-7xl mx-auto px-4 py-10">
+      {/* <h1 className="text-3xl font-bold text-gray-900 mb-8">{page.title}</h1> */}
+      <AboutUs />
     </div>
   );
 }
